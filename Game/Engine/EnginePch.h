89a7b678a33269550a9ecb547cc5791df2cc9bae
developeSHG <developeSHG@gmail.com>
2023-06-01 22:19:15 +0900

    [Engine] RootSignature를 통해 ConstandBuffer View로 렌더링

--- Game/Engine/EnginePch.h
+++ Game/Engine/EnginePch.h
@@ -1,9 +1,9 @@
 ﻿#pragma once
 
 // 각종 include
-#include <Windows.h>
+#include <windows.h>
 #include <tchar.h>
-#include <memory>>
+#include <memory>
 #include <string>
 #include <vector>
 #include <array>
@@ -30,29 +30,29 @@ using namespace Microsoft::WRL;
 #pragma comment(lib, "d3dcompiler")
 
 // 각종 typedef
-using int8		= __int8;
-using int16		= __int16;
-using int32		= __int32;
-using int64		= __int64;
-using uint8		= unsigned __int8;
-using uint16	= unsigned __int16;
-using uint32	= unsigned __int32;
-using uint64	= unsigned __int64;
-using Vec2		= XMFLOAT2;
-using Vec3		= XMFLOAT3;
-using Vec4		= XMFLOAT4;
-using Matrix	= XMMATRIX;
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using uint32 = unsigned __int32;
+using uint64 = unsigned __int64;
+using Vec2 = XMFLOAT2;
+using Vec3 = XMFLOAT3;
+using Vec4 = XMFLOAT4;
+using Matrix = XMMATRIX;
 
 enum
 {
 	SWAP_CHAIN_BUFFER_COUNT = 2
-
 };
+
 struct WindowInfo
 {
 	HWND	hwnd; // 출력 윈도우
 	int32	width; // 너비
-	int		height; // 높이
+	int32	height; // 높이
 	bool	windowed; // 창모드 or 전체화면
 };
 
@@ -62,10 +62,13 @@ struct Vertex
 	Vec4 color;
 };
 
+struct Transform
+{
+	Vec4 offset;
+};
+
 #define DEVICE			GEngine->GetDevice()->GetDevice()
 #define CMD_LIST		GEngine->GetCmdQueue()->GetCmdList()
 #define ROOT_SIGNATURE	GEngine->GetRootSignature()->GetSignature()
 
-extern unique_ptr<class Engine> GEngine;		// 전역 선언
-
-
+extern unique_ptr<class Engine> GEngine;
--- Game/Resources/Shader/default.hlsli
+++ Game/Resources/Shader/default.hlsli
@@ -1,4 +1,13 @@
 
+cbuffer TEST_B0 : register(b0)
+{
+    float4 offset0;
+};
+
+cbuffer TEST_B1 : register(b1)
+{
+    float4 offset1;
+};
 
 struct VS_IN
 {
@@ -12,18 +21,18 @@ struct VS_OUT
     float4 color : COLOR;
 };
 
-// 정점 변화에 대해 정보를 고치는 단계 
 VS_OUT VS_Main(VS_IN input)
 {
     VS_OUT output = (VS_OUT)0;
 
-    output.pos = float4(input.pos, 1.f); // 마지막만 1로 채워넣음
+    output.pos = float4(input.pos, 1.f);
+    output.pos += offset0;
     output.color = input.color;
+    output.color += offset1;
 
     return output;
 }
 
-// 색상을 정해주는 단계
 float4 PS_Main(VS_OUT input) : SV_Target
 {
     return input.color;
